edccfd79de236d67ccbd96c4d560a3f71c4cafa1
Faisal <faisal.salih.hayat768@gmail.com>
2025-03-13 23:19:06 +0500

    Fresh start

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,19 @@ const Hero = () => {
     const [loadedvideo, setLoadedvideo] = useState(0);
 
     const totalVideos = 4;
-    const nextVdRef = useRef(null);
+    const nextvideoRef = useRef(null);
+
+    // Take the source of every videos
+    const getvideosource = (index) => `videos/hero-${index}.mp4`; 
 
     const handleminiclicked = () => {
         setHasclicked(true);
 
         setcurrentIndex((prevIndex) => prevIndex + 1);
+    }
+
+    const handlevideoload = () => {
+        setLoadedvideo((prev) => prev + 1);
     }
 
   return (
@@ -21,8 +28,8 @@ const Hero = () => {
             <div>
                 <div className="bg-violet-300 mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
 
-                    <div onClick={handleminiclicked}>
-                        Minivideoplayer
+                    <div onClick={handleminiclicked} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
+                        <video ref={nextvideoRef} src={getvideosource(currentIndex + 1)} loop muted id='current-video' className='size-64 origin-center scale-150 object-cover object-center'/>
                     </div>
 
                 </div>
