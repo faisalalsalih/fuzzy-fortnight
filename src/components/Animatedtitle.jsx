9dfb297eabe682dcd77629faebe0f232147f9414
Faisal <faisal.salih.hayat768@gmail.com>
2025-04-08 11:56:30 +0500

    Left this Project for a while on 1:13

--- src/components/Animatedtitle.jsx
+++ src/components/Animatedtitle.jsx
@@ -1,23 +1,24 @@
-import React, { useRef } from 'react'
+import React, {useEffect, useRef} from 'react'
 
-const Animatedtitle = ({ title, containerClass }) => {
+const Animatedtitle = ({title, containerClass}) => {
 
     const containerRef = useRef(null);
 
+    useEffect(() => {
 
-    return (
-        <div>
-            ref={useRef}
-            className={`animated-title ${containerClass}`}>
-            {title.split("<br />").map((line, index) => (
-                <div key={index} className='flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3'>
-                    {line.split(' ').map((word, i) => (
-                        <span key={i} className="animated-word" dangerouslySetInnerHTML={{ __html: word }}></span>
-                    ))}
-                </div>
-            ))}
-        </div>
-    )
+    }, [])
+
+  return (
+    <div ref={containerRef} className={`animated-title ${containerClass}`}>
+        {title.split('<br />').map((line, index) => (
+            <div key={index} className='flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3'>
+                {line.split(' ').map((word, i) => (
+                    <span key={i} className='animated-word' dangerouslySetInnerHTML={{__html: word}}/>
+                ))}
+            </div>
+        ))}
+    </div>
+  )
 }
 
 export default Animatedtitle
