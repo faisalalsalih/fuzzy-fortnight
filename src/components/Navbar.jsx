bc01aa665d43807fac9cf409d36247fea96ee50d
Faisal <faisal.salih.hayat768@gmail.com>
2025-04-14 10:24:43 +0500

    Transition Effect Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,10 @@ import React, { useRef } from 'react'
 import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
 
+
+
+const navitems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact'];
+
 const Navbar = () => {
 
     const navConatinerRef = useRef(null);
@@ -13,6 +17,15 @@ const Navbar = () => {
         <div className='flex items-center gap-7'>
             <img src="/img/logo.png" alt="logo" className='w-10' />
             <Button id="product-button" title="Products" rightIcon={<TiLocationArrow />} containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"/>
+        </div>
+        <div className="flex h-full items-center">
+            <div className="hidden md:block">
+                {navitems.map((item) => (
+                    <a className='nav-hover-btn'>
+                        {item}
+                    </a>
+                ))}
+            </div>
         </div>
       </nav>
       </header>
