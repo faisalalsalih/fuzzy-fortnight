8b0847c6c0630503b130b2d1ffd72a255e661a45
Faisal <faisal.salih.hayat768@gmail.com>
2025-04-15 21:43:28 +0500

    use the windowscroll Property in React

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
+import { useWindowScroll } from 'react-use';
+import gsap from 'gsap';
 
 
 
@@ -39,6 +41,50 @@ const Navbar = () => {
       }
     }, [isAudioPlaying])
 
+
+
+
+    // From Here the start of windows scroll Properties
+
+    // Get the Coordinates Here
+
+    const { y: currentScrollY } = useWindowScroll();
+
+    // A useState to check where the scroll Enters
+
+    const [lastScrollY, setlastScrollY] = useState(0);
+
+
+    const [isNavvisible, setisNavvisible] = useState(true);
+
+
+
+    useEffect(() => {
+      if(currentScrollY === 0){
+        setisNavvisible(true);
+        navConatinerRef.current.classList.remove('floating-nav');
+      }
+      else if (currentScrollY > lastScrollY){
+        setisNavvisible(false);
+        navConatinerRef.current.classList.add('floating-nav');
+      }
+      else if (currentScrollY < lastScrollY){
+        setisNavvisible(true);
+        navConatinerRef.current.classList.add('floating-nav');
+      }
+
+      setlastScrollY(currentScrollY);
+    }, [currentScrollY])
+
+
+    useEffect(() => {
+      gsap.to(navConatinerRef, {
+        y: isNavvisible ? 0 : -100,
+        opacity: isNavvisible ? 1 : 0,
+        duration: 0.2 
+      })
+    }, [isNavvisible])
+
   return (
     <div ref={navConatinerRef} className='fixed inset-x-0 top-4 z-50 h-16 border-none  transition-all duration-700 sm:inset-x-6'>
       <header className='absolute top-1/2 w-full -translate-y-1/2'>
@@ -57,7 +103,7 @@ const Navbar = () => {
             </div>
 
             <button className='ml-10 flex items-center spac-x-0.5' onClick={toggleAudioIndicator}>
-              <audio ref={audioElementRef} className='hidden' src='/audio/loop.mp3' loop />
+              <audio ref={audioElementRef} className='hidden' src='/audio/emotion.mp3' loop />
               {[1, 2, 3, 4].map((bar) => (
                 <div key={bar} className={`indicator-line ${isAndicatorActive ? 'active': ''}`} style={{animationDelay: `${bar * 0.1}s`}} />
               ))}
