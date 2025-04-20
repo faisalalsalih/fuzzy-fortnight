6b34abd2f9f70ff5ced019833f17c4d566f4c6fb
Faisal <faisal.salih.hayat768@gmail.com>
2025-04-20 11:44:35 +0500

    Animated Website Completed Successfully

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,7 +27,6 @@ const Contact = () => {
         <div className="flex flex-col items-center text-center">
           <p className='font-general text-[10px] uppercase'>Join Zenry</p>
           <p className='special-font mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[6rem]'>Let's b<b>u</b>ild the <br /> new era of a g<b>a</b>mimg t<b>o</b>gether</p>
-
           <Button title="contact us" containerClass="mt-10 cursor-pointer bg-blue-50"/>
         </div>
       </div>
